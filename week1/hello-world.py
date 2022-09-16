6d6f91b5a2fe6ad606a4bd2bb705e8c0fa06b6b3
Chris Lansman <clansman@nvidia.com>
2022-09-16 10:56:35 -0500

    updating the hello world file

---
+++ week1/hello-world.py
@@ -0,0 +1,14 @@
+print ("Hello world!")
+
+# print(type("hello"))
+# my_string = "HELLO"
+# print(my_string.count("L"))
+# print(my_string.lower())
+# my_int = 1
+# my_float = 1.0
+# my_string = "string"
+# my_string2 = 'string'
+# print(type(my_int))
+# print(type(my_float))
+# print(type(my_string))
+# print(type(my_string2))
