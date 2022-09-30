4b194027a7f2a1980c6f373002cf7de6bf9a3818
Mason05Price <114709258+Mason05Price@users.noreply.github.com>
2022-09-29 20:05:12 -0500

    Adding assignment for week 3 to the repository

---
+++ student-hw/week3/Price_Mason_HW_Week3.py
@@ -0,0 +1,32 @@
+person_name = (input("Name: "))
+print("Hello", person_name, ", how old are you?")
+person_age_years = (input("Age: "))
+person_age_days = int(person_age_years)*365
+print("Thank you for letting us know that you are", person_age_days, "days old.")
+person_age_hours = int(person_age_days)*24
+person_age_minutes = int(person_age_hours)*60
+person_age_seconds = int(person_age_minutes)*60
+print("That also means that you are", person_age_hours, "hours old,", person_age_minutes, "minutes old, or", person_age_seconds, "seconds old.")
+
+
+print(99566+2321)
+print(3567*3333)
+print(3445/1111)
+print(22%2)
+    # modulo
+print(3|5)
+    # bit-wise OR
+
+
+# a) = 8
+# b) = 9
+# c) = 5
+# d) = 5
+# e) = 14
+# f) = 20
+
+
+print("The area of a rectangle is", str(int(input("Width: "))*int(input("Length: "))))
+
+
+# I couldn't perform the git push. It said "Permission to chris-lansman/python-class.git denied to Mason05Price."
