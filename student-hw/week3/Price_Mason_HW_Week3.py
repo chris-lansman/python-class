b79da589606db8d0647b53de4e67324b3f3c4b50
Mason05Pice <mason05price@gmail.com>
2022-10-01 10:11:47 -0500

    Merge branch 'main' of https://github.com/chris-lansman/python-class

--- student-hw/week3/Price_Mason_HW_Week3.py
+++ student-hw/week3/Price_Mason_HW_Week3.py
@@ -2,11 +2,13 @@ person_name = (input("Name: "))
 print("Hello", person_name, ", how old are you?")
 person_age_years = (input("Age: "))
 person_age_days = int(person_age_years)*365
-print("Thank you for letting us know that you are", person_age_days, "days old.")
 person_age_hours = int(person_age_days)*24
 person_age_minutes = int(person_age_hours)*60
 person_age_seconds = int(person_age_minutes)*60
-print("That also means that you are", person_age_hours, "hours old,", person_age_minutes, "minutes old, or", person_age_seconds, "seconds old.")
+print(f"""Thank you for letting us know that you are {person_age_days} days old.
+That also means that you are {person_age_hours} hours old, 
+{person_age_minutes} minutes old, 
+or {person_age_seconds} seconds old.""")
 
 
 print(99566+2321)
@@ -27,6 +29,3 @@ print(3|5)
 
 
 print("The area of a rectangle is", str(int(input("Width: "))*int(input("Length: "))))
-
-
-# I couldn't perform the git push. It said "Permission to chris-lansman/python-class.git denied to Mason05Price."
