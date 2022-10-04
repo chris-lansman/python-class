4a771fca72aa59a59f2ed5757ba6ca7c94ba21ad
Joshua Scaife <joshuamscaife@gmail.com>
2022-10-04 16:04:03 -0500

    Week 3 Assignment

---
+++ student-hw/week3/Scaife_Josh_HW_Week3.py
@@ -0,0 +1,10 @@
+x=(92/16**(7)-5)
+print (x)
+
+
+Facebook_Username=input ("Username:")
+Facebook_Password =input ("Password:")
+print ("Is this you? ") 
+print (Facebook_Username)
+input ("yes or no:")
+print (("Welcome to Facebook"),(Facebook_Username)) 
