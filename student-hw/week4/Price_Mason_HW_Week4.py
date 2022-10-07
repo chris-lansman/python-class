54df86da82de34ad6b8b65c90494b176ec0664d6
Mason05Pice <mason05price@gmail.com>
2022-10-06 20:42:28 -0500

    Adding week 4 HW to repository

---
+++ student-hw/week4/Price_Mason_HW_Week4.py
@@ -0,0 +1,26 @@
+# 2)
+#   a) yes
+#   b) '' or ""
+#   c) Chris
+#   d) The value of pi is 3.14
+#   e) Python can't print it out when it is a float
+# 3) It makes all the letters in the string lowercase
+# 4) It finds where the part of the string you are looking for starts
+# 5) Yes
+# 6) 0
+# 7) Taking out a specified part of the string
+#   a) s = "Hello"
+#       i) 
+#       ii) ell
+#       iii) Hel
+#       iv) Hello
+# 8) It turns it into an integer
+# 9) It turns it into a string
+# 10) s = '3'
+#   a) 3 as a string
+#   b) 3 as an integer
+#       i) It turns  the string '3' into the integer 3
+# 11) s = '3.14'
+#   a) 3.14 as a string
+#   b) It returns an error because s is not an integer, it is a float
+#   c) 3.14 as a float
