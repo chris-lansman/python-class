b5d387d93fcd327d976ff35f24008ba085cb24aa
JoshuaScaife <joshuamscaife@gmail.com>
2022-10-07 13:06:52 -0500

    Adding homework

--- student-hw/week3/Scaife_Josh_HW_Week3.py
+++ student-hw/week3/Scaife_Josh_HW_Week3.py
@@ -1,10 +1,20 @@
-x=(92/16**(7)-5)
-print (x)
 
 
-Facebook_Username=input ("Username:")
-Facebook_Password =input ("Password:")
-print ("Is this you? ") 
-print (Facebook_Username)
-input ("yes or no:")
-print (("Welcome to Facebook"),(Facebook_Username)) 
+
+
+Facebook_Username=(input ("Username: "))
+Facebook_Password =(input ("Password: "))
+Saved_Username= ("JoshuaScaife")
+Saved_Password = ("mine")
+
+if Facebook_Username == Saved_Username :print(" ")
+else: print('Unknown User')
+
+if (Facebook_Password) == (Saved_Password) :print ("Is this you? "), print (Facebook_Username)
+else: print ("Incorrect password", Facebook_Password), print ("Please re-enter username and password")
+
+
+if (input ("yes or no: ")) == "yes" :print (("Welcome to Facebook"),(Facebook_Username))
+else: print ("Please re-enter username and password")
+
+
