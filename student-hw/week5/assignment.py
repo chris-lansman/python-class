1d59d2a770875e7736dc9bc4cca75442a9ab0838
Chris Lansman <clansman@nvidia.com>
2022-10-13 21:27:16 -0500

    adding week 5 assignment

---
+++ student-hw/week5/assignment.py
@@ -0,0 +1,36 @@
+# What is an operator?
+# What is the purpose of a comparison operator and what does it do?
+# What does the ‘<’ operator do?
+# What does the ‘==’ operator do?
+# What does the ‘!=’ operator do?
+# Try running the following bits of code:
+# What does ‘10 < 30’ return?
+# What does ‘30 < 20’ return?
+# What does ‘20 <= 20’ return?
+# What does ‘20 != 29’ return?
+# What does the ‘if’ check do?
+# Copy the following code to a file and run:
+# if 10 < 20:
+#    print("Ten is less than 20!")
+# else:
+#    print("Ten is not less than 20!")
+#  Try running the following guessing game code:
+# number = 23
+# guess = int(input('Enter an integer : '))
+ 
+# if guess == number:
+#     # New block starts here
+#     print('Congratulations, you guessed it.')
+#     print('(but you do not win any prizes!)')
+#     # New block ends here
+# elif guess < number:
+#     # Another block
+#     print('No, it is a little higher than that')
+#     # You can do whatever you want in a block ...
+# else:
+#     print('No, it is a little lower than that')
+#     # you must have guessed > number to reach here
+ 
+# print('Done')
+# # This last statement is always executed,
+# # after the if statement is executed.
