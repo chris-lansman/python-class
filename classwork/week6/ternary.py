679eb16e51d7f747efdcdc24a1c2f7358bb369d2
Chris Lansman <clansman@nvidia.com>
2022-10-14 10:48:42 -0500

    adding week 6 classwork file

---
+++ classwork/week6/ternary.py
@@ -0,0 +1,17 @@
+# Python ternary operations
+# Help reduce code size and make it easier to read simple conditionals
+
+# Example of basic if check
+# a = 2
+# b = 3
+# if a > b:
+#     print("a")
+# else:
+#     print("b")
+
+# # Lets rewrite this using a ternary operator format
+# # Syntax:   [on_true] if [expression] else [on_false]
+# print ("a" if a > b else "b")
+
+# NOTE: Python does not have a ternary "Operator" like C++ (:). 
+# But it can perform ternary logical operations
