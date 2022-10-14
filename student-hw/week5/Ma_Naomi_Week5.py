feb760e2ac46a2980341dd0b9f3e5d2d16221c91
Chris Lansman <clansman@nvidia.com>
2022-10-14 10:56:15 -0500

    adding student hw assignment

---
+++ student-hw/week5/Ma_Naomi_Week5.py
@@ -0,0 +1,20 @@
+# What is an operator? Characters that represents mathmatical operations
+
+# What is the purpose of a comparison operator and what does it do? Compares two values
+
+# What does the ‘<’ operator do? The "less than" the value on the left is less than the value on the right
+
+# What does the ‘==’ operator do? The "equal to" compares two values and returns True if the left value is equal to the right value
+
+# What does the ‘!=’ operator do? The "not equal to" compares two values and returns True if the left value isn’t equal to the right value.
+ 
+# Try running the following bits of code:
+# What does ‘10 < 30’ return? True
+#   
+# What does ‘30 < 20’ return? False
+
+# What does ‘20 <= 20’ return? True
+
+# What does ‘20 != 29’ return? True
+
+# What does the ‘if’ check do? Checks the experssion using a boolean check
