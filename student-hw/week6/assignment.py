9fd7bed919f98d72f8b4a46df30f86aaadfebe59
Chris Lansman <clansman@nvidia.com>
2022-10-19 14:22:41 -0500

    updating with HW answers

---
+++ student-hw/week6/assignment.py
@@ -0,0 +1,24 @@
+# What is the purpose of the ‘else’ statement?
+#   A: Allows a second condition to to trigger in the even the first is not true
+# What is the purpose of the ‘elif’ statement in last week's code sample in problem 10?
+#   A: Offers another choice in the if statement instead of the default if/else 
+# What does a while statement do?
+#   A: Keeps a program in an execution loop until the while is exited.
+# Run the following program and tell me which statement is printed:
+#   A: C
+# How many times will this while loop repeat?
+#   A: 10
+# What does the ‘while’ keyword do?
+#   A: Keeps a program in a loop
+# What does the following code output?
+#   A: 1,2,3,4 The for loop is over
+# What does the ‘for’ keyword do?
+#   A: Creates a ranged loop
+# What does the ‘range’ keyword do?
+#   A: Creates a range which returns values in that range incrementing each time
+# What is a ‘break’ used for?
+#   A: Exit from a control statement 
+# What is a ‘continue used for?
+#   A: Loop back to the top of a control statement 
+# How many times will the following code loop?
+#   A: 10
