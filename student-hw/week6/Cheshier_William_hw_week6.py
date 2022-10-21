968d954b16f94e445e73aebabceb171553fc92d9
Chris Lansman <clansman@nvidia.com>
2022-10-21 11:10:27 -0500

    adding student HW to repo:

---
+++ student-hw/week6/Cheshier_William_hw_week6.py
@@ -0,0 +1,41 @@
+#3: The 'else' statement initiates code based on the response recieved from the last command.
+
+#4: The "elif" statement is used if you want code initiated based off the command of the previous "else" statement.
+
+#5: The "while" evaluates code against Boolean, and if it is true it continues.
+
+# #6: number = 22
+# if number > 22:
+#     print("A")
+# elif number == 23:
+#     print("B")
+# elif number >= 22:
+#     print("C")---"C" is printed
+
+# #7: loops = 20
+# number_loops = 1
+# while loops != 0:
+#     if loops % 2 == 0:
+#         loops = loops - 2
+#     print (number_loops)
+#     number_loops += 1---It will loop 10 times.
+
+#8: The "while" keyword starts a while loop.
+
+# #9: for i in range(1, 5):
+#     print(i)
+# else:
+#     print('The for loop is over')---Returns 1, 2, 3, 4, The loop is over.
+
+#10: The "for" keyword starts a for loop.
+
+#11: The "range" function returns numbers before stopping before the specified one.
+ 
+#12: The "break" statement breaks out of a loop.
+
+#13: The "continue" statement tells Python to continue past the rest of the code in this loop and move on to the next statement.
+
+# #14: for i in range(1, 100):
+#     print(i)
+#     if i > 10:
+#         break---It will loop 11 times, then break.
---
+++ student-hw/week6/Worthington_samuel_hw_week6.py
@@ -0,0 +1,12 @@
+#The purpose of the else statement is to prepare for a result other than what was expected for example a login screen if you enter a wrong password or username it may say invalid password.
+#Elif is a merge of else and if. meaning that if an answer isnt what was expected then there would be another if.
+#while means that while set conditions are met the code will repeat in a loop ( when using loop)
+#B
+#10 times
+#the sime as the while statement?
+# 1 2 3 4 the loop is over
+# for repeats a loop a certain amount of times
+#range means that something is in between the two number the first one is inclusive the last one isnt
+#breaks a loop
+# Returns to the beggining of a while loop
+#10 times
