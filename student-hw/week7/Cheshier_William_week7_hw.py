f2da9caf019ee26a87cedf15446a0bbdc3d1f22e
Chris Lansman <clansman@nvidia.com>
2022-10-27 14:40:46 -0500

    adding student HW

---
+++ student-hw/week7/Cheshier_William_week7_hw.py
@@ -0,0 +1,36 @@
+#3: A list is a data structure that holds an ordered collection of items.
+
+#4: {'turkey', 'chicken', 'bacon', 'sausage'}
+
+#5: Many different types of data.
+
+# #6: my_list = ['a', 3, [2, 1], "hello"]
+# print(my_list[0])
+# print(my_list[1])
+# print(my_list[2])
+# print(my_list[3])---returns a 3 [2,1] hello
+
+# #7: my_list = ['a', 3, [2, 1], "hello"]
+# print(type(my_list[0]))
+# print(type(my_list[1]))
+# print(type(my_list[2]))
+# print(type(my_list[3]))---returns <class 'str'> <class 'int'> <class 'list'> <class 'str'>
+
+#8: Tuples are ordered collections of data.
+
+#9: No, you cannot change its values.
+
+# #10: my_tuple = ("hello", "world")
+# my_tuple[0] = "new string!"---returns as an error because you cannot change tuple values once they have been assigned.
+
+#11: A dictionary is an unordered collection of values.
+#11 a: A key is a set that is used to identify records in table.
+#11 b: A value returns as a "view object"
+
+# #12: my_dictionary = {"chris": 36, "bob": 27}
+# print(my_dictionary["chris"])---returns 36
+
+# #13: my_dictionary = {"chris": 36, "bob": 27}
+# print(my_dictionary[36])---it returns as a "key error"
+
+#14: shoplist = ['apple', 'mango', 'carrot', 'banana']---does not have an output because we are only assigning a value, not printing anything.
