267b898d4eb2b8dec56000a90619bb11a72e90c5
Mason05Pice <mason05price@gmail.com>
2022-11-03 22:15:44 -0500

    Adding week 8 HW

---
+++ student-hw/week8/Price_Mason_HW_Week8.py
@@ -0,0 +1,28 @@
+# 1)
+# 2) A reusable peice of a program
+# 3) hello world
+#    hello world
+# 4) values you supply to the function so that the function can do something utilising those values
+# 5) '4 is a maximum'
+# def print_max(a, b):
+#     if a > b:
+#         print(a, 'is maximum')
+#     elif a == b:
+#         print(a, 'is equal to', b)
+#     else:
+#         print(b, 'is maximum')
+ 
+# # directly pass literal values
+# print_max(3, 4)
+# 6) A variable that is declared inside a funtion definition
+#   a) The block the variable is declared in
+# 7) 
+    # x = 50
+ 
+    # def func(x):
+    #     print('x is', x)
+    #     x = 2
+    #     print('Changed local x to', x)
+ 
+    # func(x)
+    # print('x is still', x)
