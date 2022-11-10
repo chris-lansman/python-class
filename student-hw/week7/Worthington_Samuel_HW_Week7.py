bd1576ce32b8f3dba175e8836c45c8cd278ca7b7
Chris Lansman <clansman@nvidia.com>
2022-11-10 15:16:18 -0600

    adding student HW

---
+++ student-hw/week7/Worthington_Samuel_HW_Week7.py
@@ -0,0 +1,13 @@
+#a list is a way to store lots of different data. a list also keeps the same order
+# snippet of code that creates a list: []
+#lists can hold many types of data
+# the following code prints s a 3 [2, 1] hello
+#prints out str int list str
+# a tuple is immutabe object cannont be changed
+# you cannot modify tuple tuple is like turtle : stronk
+# it outputs tuple doesnt support item reassignment
+# a dictionary is a fancy array a key is an input a value is whats associated with the key
+# the following code outputs 36
+# what happens is a key error
+# the following code outputs item 0 is apple item 1 is mango item 2 is carrot item 3 is banana item -1 is banana item -2 is carrot
+ 
---
+++ student-hw/week8/Samuel_worthington_HW_WEEK8.py
@@ -0,0 +1,7 @@
+# a function is a reusable piece of programs
+# hello world would be printed twice
+#the input of a function
+#b is maximum
+#A variable only used in a certain function 
+# the range of the variable, where its located
+# only took effect in the function.
